45bff1ea25ed17df5814e7d5e2a5122065825046
Alexandr Kolosov <akolosov@mirapolis.ru>
2012-12-05 16:29:18 +0400

    more general exception handling

--- src/main/scala/org/eordie/lib/UploadManager.scala
+++ src/main/scala/org/eordie/lib/UploadManager.scala
@@ -4,16 +4,20 @@ import net.liftweb.http._
 import net.liftweb.http.rest.RestHelper
 import net.liftweb.json.JsonAST.JValue
 import net.liftweb.json.JsonDSL._
-import net.liftweb.common.{Loggable, Box}
+import net.liftweb.common._
 import net.liftweb.util.StringHelpers
 import com.mongodb.gridfs.{GridFSInputFile, GridFSDBFile}
-import com.mongodb.{MongoException, BasicDBObject}
+import com.mongodb.BasicDBObject
 import net.liftweb.http.BadResponse
 import net.liftweb.http.StreamingResponse
 import net.liftweb.http.InMemoryResponse
 import org.eordie.config.MongoConfig
 import org.eordie.model.car.Offer
-import com.mongodb.MongoException.Network
+import net.liftweb.util.ControlHelpers._
+import net.liftweb.http.StreamingResponse
+import net.liftweb.http.InMemoryResponse
+import net.liftweb.common.Full
+import net.liftweb.http.BadResponse
 
 /**
  * Менеджер для загрузки файлов
@@ -76,17 +80,18 @@ object UploadManager extends RestHelper with Loggable {
   }
 
   def stream(imageName: String, contentTypeFunc: (GridFSDBFile) => List[(String, String)]): () => Box[LiftResponse] = {
-    var image: GridFSDBFile = null
-    try {
-      image = MongoConfig.mongoGridFS.findOne(imageName)
-    } catch {
-      case e: Exception => logger.info("Can't find image", e)
-    }
+    tryo(MongoConfig.mongoGridFS.findOne(imageName)) match {
+      case Full(image) => {
+        val imageStream = image.getInputStream
+        StreamingResponse(imageStream, () => imageStream.close(), image.getLength, contentTypeFunc(image), Nil, 200)
+      }
 
-    if (image == null) new BadResponse
-    else {
-      val imageStream = image.getInputStream
-      StreamingResponse(imageStream, () => imageStream.close(), image.getLength, contentTypeFunc(image), Nil, 200)
+      case Empty => new BadResponse
+
+      case Failure(_, ex, _) => {
+        logger.info("Can't find image", ex.openTheBox)
+        new BadResponse
+      }
     }
   }
 }
